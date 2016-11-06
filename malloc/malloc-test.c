118c0f84b02caa1947193eb8373fa9b6c2c71816
Savan <savanpatel3@gmail.com>
2016-11-06 13:47:25 -0500

    Completed basic code to perform nice thread testing and collecting stats in result.out

--- malloc/malloc-test.c
+++ malloc/malloc-test.c
@@ -2,12 +2,17 @@
 #include <pthread.h>
 #include <time.h>
 #include <stdlib.h>
-
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <math.h>
+#include <string.h>
 
 struct thread_args
 {
     int allocation_size;
     int array_size;
+    int thread_count;
 };
 
 // call this function to start a nanosecond-resolution timer
@@ -25,7 +30,9 @@ long timer_end(struct timespec start_time)
 {
     struct timespec end_time;
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end_time);
-    long diffInNanos = end_time.tv_nsec - start_time.tv_nsec;
+    long diffInNanos =
+    (round(end_time.tv_nsec/ 1.0e6) -
+                        round(start_time.tv_nsec/ 1.0e6));
     return diffInNanos;
 }
 
@@ -34,16 +41,16 @@ long timer_end(struct timespec start_time)
  *  testing will start with size = 1 KB upto  2^n < MAX-SIZE.
  *  it is recommended to have MAX-SIZE in power of 2.
  */
-int MAX_SIZE = 1024;
+int MAX_SIZE = 1024 * 1024;
 
 
 
 /*
  *  Test thread will start for allocated array size = 1 and iteratvely
- *  allocate n number of size of array pointers followed by that much
+ *  allocate n number of size of array pointers followed by that many
  *  free() call till MAX_ALLOCATION_ARRAY_SIZE.
  */
-int MAX_ALLOCATION_ARRAY_SIZE = 100;
+int MAX_ALLOCATION_ARRAY_SIZE = 500;
 
 
 /*
@@ -59,6 +66,12 @@ char TEST_OUT_FILE[] = "result.out";
 int MAX_THREADS = 500;
 
 
+
+/*
+ * out file to store stats.
+ */
+
+FILE *result_file;
 pthread_mutex_t result_file_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 
@@ -68,13 +81,14 @@ pthread_mutex_t result_file_mutex = PTHREAD_MUTEX_INITIALIZER;
  */
 void * nice_thread_run(void *args)
 {
-    int allocation_size, array_size;
+    int allocation_size, array_size, thread_count;
     allocation_size = ((struct thread_args *)args)->allocation_size;
     array_size = ((struct thread_args *)args)->array_size;
+    thread_count = ((struct thread_args *)args)->thread_count;
 
     int i = 0;
     void **test_arr;
-    long time_elapsed_nanos;
+    long time_elapsed_ms;
     struct timespec starttime = timer_start();
 
     test_arr = malloc(array_size * sizeof(void*));
@@ -84,28 +98,37 @@ void * nice_thread_run(void *args)
         test_arr[i] = malloc(allocation_size);
         if(NULL == test_arr)
         {
-           printf("\nNo memory!\n");
            break;
         }
     }
 
-    time_elapsed_nanos = timer_end(starttime);
+    time_elapsed_ms = timer_end(starttime);
 
     for(i = 0; i < array_size; i++)
     {
         if(NULL != test_arr[i])
-        free(test_arr[i]);
+        {
+          free(test_arr[i]);
+        }
     }
 
     free(test_arr);
-    printf("\n %ld", time_elapsed_nanos);
+    pthread_mutex_lock(&result_file_mutex);
+    fprintf(result_file,
+            "%d\t%d\t%d\t%ld\n",
+            thread_count,
+            allocation_size,
+            array_size,
+            time_elapsed_ms);
+    pthread_mutex_unlock(&result_file_mutex);
+    return NULL;
+    //printf("\n %ld", time_elapsed_nanos);
 }
-
 
 
 int main()
 {
-    FILE *result_file = fopen(TEST_OUT_FILE, "w+");
+    result_file = fopen(TEST_OUT_FILE, "w+");
 
     int thread_count = 50;
     int i = 0;
@@ -114,9 +137,9 @@ int main()
     int allocation_size = 8;
     struct thread_args arg;
 
-    while (thread_count < MAX_THREADS)
+    while (thread_count <= MAX_THREADS)
     {
-      printf("\n Thread count, n = %d", thread_count);
+      printf(" Thread count, n = %d \n", thread_count);
       while(allocation_size < MAX_SIZE)
       {
             array_size = 1;
@@ -124,7 +147,7 @@ int main()
             {
                 arg.allocation_size = allocation_size;
                 arg.array_size = array_size;
-
+                arg.thread_count = thread_count;
                 for(i = 0; i < thread_count; i++)
                 {
                     pthread_create(&tid[i], NULL, nice_thread_run, (void *)&arg);
